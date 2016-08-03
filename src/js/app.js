acde8ad165479ee51afb9cf9bfa108534599601b
Denis Michailov <denis.michailov@gmail.com>
2016-08-03 14:28:57 +1000

    Can Choose The Team Now

--- src/js/app.js
+++ src/js/app.js
@@ -23,9 +23,10 @@ var width = 800,
 
 // PROCESS UTILITES
 var position = { G:"Goalkeeper", D:"Defender", M:"Midfielder", F:"Forward" };
+// var columns = ["No", "Name", "Pos"];
 var columns = [];
 var data = [];
-var team = [];
+var teams = [];
 
 
 // INITIALIZATIONS
@@ -37,12 +38,22 @@ var table = d3.select('#roster')
 var thead = table.append('thead').append('tr');
 var tbody = table.append('tbody');
 
+var teamSelector = d3.select("#page-title")
+	.append("select")
+	.on("change", function() { selectTeam( this.value ); })
+	.attr("id", "team-selector");
+
 // RELOAD DATA
 var reload = function(){
 	d3.tsv('eng2-rosters.tsv', function(rows){
 		data = rows;
 		data.forEach(function(d){
 			d.Pos = position[d.Pos];
+			
+			if(teams.indexOf(d.TeamID) < 0) {
+				teams.push(d.TeamID);
+				teams[d.TeamID] = d.Team;
+			};
 		});
 		// d3.map от data[0] берет первый элемент из data и превращает его в объект d3
 		// где в его свойстве _ (underscore) хранятся данные и еще есть методы из d3
@@ -52,12 +63,20 @@ var reload = function(){
 		// в итоге мы получаем массив columns с заголовками колонок
 		// и массив объектов data со всеми данными
 		// теперь можно перерисовывать
-		redraw();
+		teamSelector.selectAll("option")
+			.data(teams)
+			.enter()
+			.append("option")
+			.attr("value", function(d){ return d; })
+			.text(function(d){ return teams[d]; })
+			.sort(function(a, b){ return d3.ascending(a, b); });
+		
+		selectTeam("afc-wimbledon");
 	});
 };
 
 // REDRAW TABLE IN BROWSER
-var redraw = function(){
+var redraw = function(roster){
 	// заполняем заголовки
 	thead.selectAll("th")
 		.data(columns)
@@ -66,7 +85,7 @@ var redraw = function(){
 		.text(function(d){ return d; });
 
 	var rows = tbody.selectAll("tr")
-		.data(data);
+		.data(roster);
 
 	rows.enter().append("tr");
 	rows.exit().remove();
@@ -85,6 +104,19 @@ var redraw = function(){
 		});
 	cells.enter().append("td");
 	cells.text(function(d){ return d; });
+
+
+
+};
+
+
+var selectTeam = function(teamId) {
+	var roster = data.filter(function(d) {
+		return d["TeamID"] == teamId;
+	});
+	d3.select("#team-name").text(teams[teamId] + " Roster");
+	document.getElementById('team-selector').value = teamId;
+	redraw(roster);
 };
 
 reload();
