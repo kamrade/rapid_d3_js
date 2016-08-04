fc8c27bc70c564ded0ebc2b20675c10181f43723
Denis Michailov <denis.michailov@gmail.com>
2016-08-04 17:25:45 +1000

    Graphics

--- src/js/app.js
+++ src/js/app.js
@@ -11,126 +11,73 @@ var $c_white = '#fff',
 
 console.log( "D3 version: " + d3.version );
 
-var width = 800,
-	height = 600;
 
-// START HERE
-// **********************************************************
-// ==========================================================
+// **********************************************   GLOBAL VARIABLES   ****************************
+var width = 750,
+	height = 300,
+	margin = {top: 20, right: 20, bottom: 20, left: 70};
 
-// OPTIONS
+// **********************************************   CONNECT TO DOM   ****************************
+var svg = d3.select("#results")
+	.append("svg")
+	.attr("width", width)
+	.attr("height", height)
+;
 
-// PROCESS UTILITES
-var position = { G:"Goalkeeper", D:"Defender", M:"Midfielder", F:"Forward" };
-// var columns = ["No", "Name", "Pos"];
-var columns = [];
-var data = [];
-var teams = [];
+// **********************************************   SCALING   ****************************
+var x = d3.scale
+	.ordinal()
+	.rangeRoundBands([margin.left, width - margin.right], 0.1);
 
+var y = d3.scale
+	.linear()
+	.range([height - margin.bottom, margin.top]);
 
-// INITIALIZATIONS
-var table = d3.select('#roster')
-	.append('table')
-	// добавляет класс
-	.classed('table', true);
-	// .classed('table-striped', true);
+var xAxis = d3.svg.axis().scale(x).orient("bottom");
+var yAxis = d3.svg.axis().scale(y).orient("left");
 
-var thead = table.append('thead').append('tr');
-var tbody = table.append('tbody');
-
-// добавление фильтра select и обработчика, который при изменении значения
-// фильтрует содержимое таблицы
-var teamSelector = d3.select("#page-title")
-	.append("select")
-	.on("change", function() { selectTeam( this.value ); })
-	.attr("id", "team-selector");
-
-// RELOAD DATA
-var reload = function(){
-	d3.tsv('eng2-rosters.tsv', function(rows){
-		data = rows;
-		data.forEach(function(d){
-			d.Pos = position[d.Pos];
-			
-			if(teams.indexOf(d.TeamID) < 0) {
-				teams.push(d.TeamID);
-				teams[d.TeamID] = d.Team;
-			};
-		});
-		// d3.map от data[0] берет первый элемент из data и превращает его в объект d3
-		// где в его свойстве _ (underscore) хранятся данные и еще есть методы из d3
-		// например keys - который достает из этого объекта все названия полей
-		// таким образом мы получаем заголовки колонок
-		columns = d3.map(data[0]).keys();
-		// в итоге мы получаем массив columns с заголовками колонок
-		// и массив объектов data со всеми данными
-		// теперь можно перерисовывать
-		teamSelector.selectAll("option")
-			.data(teams)
-			.enter()
-			.append("option")
-			.attr("value", function(d){ return d; })
-			.text(function(d){ return teams[d]; })
-			.sort(function(a, b){ return d3.ascending(a, b); });
-
-		selectTeam("afc-wimbledon");
+// **********************************************   RELOAD   ****************************
+var reload = function() {
+	var data = [];
+	d3.csv('afcw-results.csv', function(rows){
+		redraw(rows);
 	});
 };
 
-// REDRAW TABLE IN BROWSER
-var redraw = function(roster){
-	// заполняем заголовки
-	thead.selectAll("th")
-		.data(columns)
-		.enter()
-		.append("th")
-		// Обработчик события по клику по <TH> - заголовку колонки
-		// Сортировка
-		.on("click", function(d){
-			tbody.selectAll("tr")
-				.sort(function(a, b){
-					return (d === 'No')
-					? d3.ascending(+a[d], +b[d])
-					: d3.ascending(a[d], b[d]);
-				})
-				.style('background-color', function(d, i){
-					return(i%2)? 'white':'lightgray';
-				});
-		})
-		.text(function(d){ return d; });
+// **********************************************   REDRAW   ****************************
+var redraw = function(data) {
+	x.domain(data.map(function(d,i){ return i; }))
+	y.domain([0, d3.max(data, function(d){return d.GoalsScored })]);
 
-	var rows = tbody.selectAll("tr")
-		.data(roster);
+	var bars = svg.selectAll("rect.bar")
+		.data(data);
+	bars.enter()
+		.append("rect")
+		.classed("bar", true);
+	bars
+		.attr("x", function(d, i) { return x(i); })
+		.attr("width", x.rangeBand())
+		.attr("y", function(d){ return y(d.GoalsScored); })
+		.attr("height", function(d) { return y(0) - y(d.GoalsScored); });
 
-	rows.enter().append("tr")
-		.style('background-color', function(d, i){
-			return(i%2)? 'white':'lightgray';
-		});
-	rows.exit().remove();
-
-	// Мы наполняем переменную cell tr-ками(rows), а каждую tr-ку в свою очередь забиваем td-шками
-	// Причем в каждую tr кладем столько td, сколько у нас columns-ов, тоесть колонок.
-	// Получаем полную структуру, которую потом заливаем текстом.
-	var cells = rows.selectAll("td")
-		.data(function(row){
-			var values = [];
-			columns.forEach(function(d){
-				values.push(row[d]);
-			});
-			return values;
-		});
-	cells.enter().append("td");
-	cells.text(function(d){ return d; });
-};
-
-// Функция фильтра выбора команды
-var selectTeam = function(teamId) {
-	var roster = data.filter(function(d) {
-		return d["TeamID"] == teamId;
+	// AXIS DATA
+	var axisData = [
+		{axis: xAxis, dx: 0, dy: (height - margin.bottom), clazz: 'x'},
+		{axis: yAxis, dx: margin.left, dy: 0, clazz: 'y'}
+	];
+	var axis = svg.selectAll("g.axis")
+		.data(axisData);
+	axis.enter().append("g")
+		.classed("axis", true);
+	axis.each(function(d){
+		d3.select(this)
+			.attr("transform", "translate(" + d.dx + "," + d.dy + ")")
+			.classed(d.clazz, true)
+			.call(d.axis);
 	});
-	d3.select("#team-name").text(teams[teamId] + " Roster");
-	document.getElementById('team-selector').value = teamId;
-	redraw(roster);
+
+
 };
 
 reload();
+
